b51a2ca349125373bb93b1bb8130c3b7d1c32948
Bhanushree <bhanushree@twvending.com>
2019-01-07 16:04:39 -0600

    Save SMS CALL fix

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,14 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+message: any; 
   constructor(public navCtrl: NavController) {
-
+this.message = '';
   }
 
+  callMe(){
+   // let num = "7158213965";
+    window.open("sms:7158213965" + this.message, "_system")
+
+  }
 }
